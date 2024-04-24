b33364683acc95f874d78be8ef6bc072758d5fa6
Anton Artikov <arti-anton@yandex.ru>
2024-04-24 20:52:44 +0300

    добавил связь пользователя с чатом, если он ранее в нем не состоял

--- internal/service/chat/connect_chat.go
+++ internal/service/chat/connect_chat.go
@@ -1,6 +1,7 @@
 package chat
 
 import (
+	"context"
 	"github.com/mistandok/chat-server/internal/model"
 	"github.com/mistandok/chat-server/internal/service"
 	"github.com/mistandok/chat-server/internal/service/chat/stream"
@@ -8,13 +9,19 @@ import (
 
 // ConnectChat ..
 func (s *Service) ConnectChat(in *model.ConnectChatIn, stream service.StreamChatMessageSender) error {
+	s.logger.Debug().Msg("попытка установить соединение с чатом")
+
 	userInChat, err := s.chatRepo.IsUserInChat(stream.Context(), in.ChatID, in.UserID)
 	if err != nil {
+		s.logger.Err(err).Msg("не удалось проверить, что пользователь находится в чате")
 		return err
 	}
 
 	if !userInChat {
-		return service.ErrUserNotInTheChat
+		err = s.createUserIfIsNeedAndLinkWithChat(stream.Context(), in.ChatID, in.UserID)
+		if err != nil {
+			return err
+		}
 	}
 
 	chatID := model.ChatID(in.ChatID)
@@ -25,6 +32,7 @@ func (s *Service) ConnectChat(in *model.ConnectChatIn, stream service.StreamChat
 
 	chatMessageChannel, ok := s.chatsMessageChannel.GetChannelForChat(chatID)
 	if !ok {
+		s.logger.Err(err).Msg("для чата не найден канал сообщений")
 		return service.ErrChatNotFound
 	}
 
@@ -32,6 +40,26 @@ func (s *Service) ConnectChat(in *model.ConnectChatIn, stream service.StreamChat
 	chat.SetStreamForUser(user, stream)
 
 	return s.processingChatChannel(chatMessageChannel, chat, user, stream)
+}
+
+func (s *Service) createUserIfIsNeedAndLinkWithChat(ctx context.Context, chatID int64, userID int64) error {
+	err := s.txManager.ReadCommitted(ctx, func(ctx context.Context) error {
+		var txErr error
+		txErr = s.userRepo.CreateMass(ctx, []int64{userID})
+		if txErr != nil {
+			s.logger.Err(txErr).Msg("не удалось создать пользовтеля")
+			return txErr
+		}
+
+		txErr = s.chatRepo.LinkChatAndUsers(ctx, chatID, []int64{userID})
+		if txErr != nil {
+			s.logger.Err(txErr).Msg("не удалось связать пользователя с чатом")
+		}
+
+		return nil
+	})
+
+	return err
 }
 
 func (s *Service) processingChatChannel(
@@ -52,6 +80,7 @@ func (s *Service) processingChatChannel(
 					continue
 				}
 				if err := userStream.Send(message); err != nil {
+					s.logger.Err(err).Msg("не удалось отправить сообщение в user stream")
 					return err
 				}
 			}
